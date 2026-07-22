8d5e1fdf7e8b3966959bf5054348c27392e253a4
TheChillMan25 <licsmanpeter@gmail.com>
2026-07-22 10:16:24 +0200

    fix: project structure

---
+++ TCML Class library/ModDetails.cs
@@ -0,0 +1,37 @@
+﻿namespace TCML_Class_library
+{
+    public class ModDetails : ModSearchResult
+    {
+        public string Description { get; set; }
+        public List<ModVersion> Versions{ get; set; }
+    }
+
+    public class ModVersion
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ProjectId { get; set; }
+        public string VersionNumber { get; set; }
+        public string VersionType { get; set; }
+        public uint Downloads { get; set; }
+        public List<ModFile> Files { get; set; }
+        public List<ModDependency> Dependencies { get; set; }
+    }
+
+    public class ModFile
+    {
+        public string Url { get; set; }
+        public bool Primary { get; set; }
+        public string FileName { get; set; }
+        public uint Size { get; set; }
+    }
+
+    public class ModDependency
+    {
+        public string VersionId { get; set; }
+        public string ProjectId { get; set; }
+        public string FileName { get; set; }
+        public string DependencyType { get; set; }
+
+    }
+}
---
+++ TCML Class library/ModSearchResult.cs
@@ -0,0 +1,22 @@
+﻿namespace TCML_Class_library
+{
+    public class ModSearchResult
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Author { get; set; }
+        public string IconUrl { get; set; }
+        public uint DownloadCount { get; set; }
+        public ModSource Source { get; set; }
+        public string Client_Side { get; set; }
+        public string Server_Side { get; set; }
+        public List<string> Categories { get; set; }
+    }
+
+    public enum ModSource
+    {
+        Modrinth,
+        CurseForge
+    }
+}
